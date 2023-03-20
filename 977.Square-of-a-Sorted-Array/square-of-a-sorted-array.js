42a08f3c1b4335786676b151abc9b5b06cff40ed
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-03-20 19:41:03 +1300

    sorted array finished

--- 977.Square-of-a-Sorted-Array/square-of-a-sorted-array.js
+++ 977.Square-of-a-Sorted-Array/square-of-a-sorted-array.js
@@ -1,13 +1,24 @@
 // Given an integer array nums sorted in non-decreasing order, 
 // return an array of the squares of each number sorted in non-decreasing order.
 
-const sortedSquares = function(nums) {
-    const sortedNums = nums.sort(function(a, b) {return a-b});
-    for (let i = 0; i <= sortedNums.length; i++) {
-        let squaredItem = sortedNums[i]*sortedNums[i];
-        array[i] = squaredItem;
+const sortedSquares = function(array) {
+    // If the input in empty
+    if(array.length<0){
+        return 0;
     }
+    // If the input contains only one number
+    if(array.length==1){
+        array[0]=array[0]*array[0];
+        return array;
+    }
+    // If the input contains more than one number
+    for (let i=0; i<array.length;i++){
+        let temp = array[i]*array[i];
+        array[i]=temp;
+    }   
+    array.sort(function(a,b){return a-b});
     return array;
 };
 
-nums = [-7,-3,2,3,11]
+// Example of an input array
+// array = [-7,-3,2,3,11]
