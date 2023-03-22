24fdbe0d233ee1a8cdd98c50e39a9014704dfd04
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-03-22 13:57:45 +1300

    runtime issue solved in Two Sum

--- 167.Two-Sum/two-sum.js
+++ 167.Two-Sum/two-sum.js
@@ -2,24 +2,63 @@
 // Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
 
 // Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
-
 // The tests are generated such that there is exactly one solution. You may not use the same element twice.
 
 // Your solution must use only constant extra space.
+
 
 // SOLUTION 1:
 // Pick the first item from numbers, subtract it to the target, and ask if the result is included in the array.
     // If it's not included, continue
     // If it's included, find the indexes of both items, add 1, and create an array with those values
+// This solution doesn't meet the runtime complexity
 
-const twoSum = function(numbers, target) {
+const twoSum1 = function(numbers, target) {
     for (let i=0; i <= numbers.length; i++){
-        let difference = target - numbers[i]
-        if(!numbers.includes(difference)){
+        let numberToFind = target - numbers[i]
+        if(!numbers.includes(numberToFind)){
             continue
         }
         const index1 = numbers.indexOf(numbers[i]) + 1;
-        const index2 = numbers.lastIndexOf(difference) + 1;
+        const index2 = numbers.lastIndexOf(numberToFind) + 1;
         return [index1, index2];
     }  
+};
+
+
+// SOLUTION 2:
+// For this solution I will use BinarySearch, and for that I created the binarySearch function.
+
+const twoSum = function(numbers, target) {
+    let answerArray = [];
+    for (let i = 0; i < numbers.length; i++) {
+        const numberToFind = target - numbers[i];
+        const trialIndex = binarySearch(numbers, numberToFind);
+        if (numbers[trialIndex] === numberToFind) {
+            if (trialIndex === i) {
+                answerArray[0] = i + 1;
+                answerArray[1] = trialIndex + 2;
+            } else {
+                answerArray[0] = i + 1;
+                answerArray[1] = trialIndex + 1;
+            }
+            break;
+        }
+    }
+    return answerArray;
+};
+
+const binarySearch = (numbers, numberToFind) => {
+    let leftLimitIndex = 0;
+    let rightLimitIndex = numbers.length;
+
+    while (leftLimitIndex < rightLimitIndex) {
+        const middleIndex = leftLimitIndex + Math.floor( (rightLimitIndex - leftLimitIndex) / 2 );
+        if (numbers[middleIndex] >= numberToFind) {
+            rightLimitIndex = middleIndex;
+        } else {
+            leftLimitIndex = middleIndex + 1;
+        }
+    }
+    return leftLimitIndex;
 };
