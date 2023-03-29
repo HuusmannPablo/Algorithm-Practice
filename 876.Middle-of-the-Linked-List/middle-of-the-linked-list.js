924b427d74f149eb0ed612fc836bbf7e04da5a56
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-03-29 16:28:01 +1300

    solution 1 added, working on solution 2 of 876.

--- 876.Middle-of-the-Linked-List/middle-of-the-linked-list.js
+++ 876.Middle-of-the-Linked-List/middle-of-the-linked-list.js
@@ -11,3 +11,26 @@
 // Input: head = [1,2,3,4,5,6]
 // Output: [4,5,6]
 // Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+// Although the input is presented as an array, it is actually a linked list!
+// SOLUTION 1: (for arrays)
+// const middleNode = function(head) {
+//     let length = head.length;
+//     const middleIndex = Math.floor(length / 2);
+//     const slicedHead = head.slice(middleIndex);
+//     return slicedHead
+// };
+
+// middleNode([1,2,3,4,5]);
+// middleNode([1,2,3,4,5,6]);
+
+// SOLUTION 2: (for Single Linked Lists)
+const middleNode = function(head) {
+    let nodeCount = 0;
+    while (head.next !== null) {
+        if() {
+            
+        }
+        nodeCount
+    }
+};
