445585fce1e731316ff0aafc38b774ec62d8d5b2
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-03-31 17:33:54 +1300

    exercise 19 solved with two pointers

--- 19.Remove-nth-Node-From-End_of-List/remove-nth-node-from-end-of-list.js
+++ 19.Remove-nth-Node-From-End_of-List/remove-nth-node-from-end-of-list.js
@@ -12,6 +12,34 @@
 // Input: head = [1,2], n = 1
 // Output: [1]
 
+// SOLUTION 1: with the use of two pointers
 const removeNthFromEnd = function(head, n) {
-    
+   let fastPointer = head;
+   let slowPointer = head;
+   
+    // I drive the fast pointer from the start (head of the linked list)
+    // to the nth node
+    for (let i = 0; i < n; i++) {
+        fastPointer = fastPointer.next;
+    }
+
+    // For the boundary case where n = length of the Linked List, the fastPointer
+    // will fall outside the bounderies of the LL, and this means we need to delete
+    // the first node
+    if(!fastPointer) {
+        return head.next;
+    }
+
+    // Now we drive the two pointers at the same "speed" to the endo of the LL
+    // Obviously the fastPointer will reach that place in length - n times, 
+    // which means the slowPointer will be seating on the node we need to delete
+    while(fastPointer && fastPointer.next) {
+        fastPointer = fastPointer.next;
+        slowPointer = slowPointer.next;
+    }
+
+    // Now we delete the node
+    slowPointer.next = slowPointer.next.next;
+
+    return head;
 };
