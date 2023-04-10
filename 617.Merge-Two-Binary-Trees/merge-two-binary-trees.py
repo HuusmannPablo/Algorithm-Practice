a94d69ab95b9d67491320fb547374176abd1a39e
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-04-10 17:46:20 +1200

    solution to tree merging using python added

--- 617.Merge-Two-Binary-Trees/merge-two-binary-trees.py
+++ 617.Merge-Two-Binary-Trees/merge-two-binary-trees.py
@@ -1,3 +1,28 @@
+# Definition for a binary tree node in Python.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# This is the Python solution to the challenge, using the same strategy as in the JavaScript solution.
+
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        return
+
+        def depthFirstSearch(r1, r2):
+            if r1 == None and r2 == None:
+                return None
+            
+            if r1 == None:
+                return r2
+            if r2 == None:
+                return r1
+            
+            r1.val += r2.val
+            r1.left = depthFirstSearch(r1.left, r2.left)
+            r1.right = depthFirstSearch(r1.right, r2.right)
+
+            return r1
+        return depthFirstSearch(root1, root2)
+    
