3647050c0f2cdd8c27d95275463d812d87cd59fc
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-04-17 17:17:26 +1200

    comments problem 344 added

--- 344.Reverse-Array/reverse-array.py
+++ 344.Reverse-Array/reverse-array.py
@@ -1,7 +1,14 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
+
+        # For each element in the array's index:
         for i in range(len(s)):
+
+            # Save the character
             character = s[0]
+            # Remove the first element of the array (first character)
             s.pop(0)
+            # Insert it back, counting i indexes from the end of the array
             s.insert(len(s) - i, character)
         return s
+    
