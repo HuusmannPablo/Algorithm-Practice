fe43df8cb9f8ced0bc489f5c02444495e3bdba42
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-04-18 16:36:38 +1200

    comments in Python solution for problem 567 added

--- 567.Permutation-In-String/permutation-in-string.py
+++ 567.Permutation-In-String/permutation-in-string.py
@@ -1,8 +1,16 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        # Firstly, I split the string to generate an array of the words
         arrayOfStrings = s2.split(" ")
+
+        # I reverse each of the words that are in the array
         reversedArray = [word[::-1] for word in arrayOfStrings]
+
+        # I join every element of the array
         reverseds2 = " ".join(reversedArray)
+
+        # If s1 is contained in s2 or its reversed version, then return True, otherwise False
         if s1 in s2 or s1 in reverseds2:
             return True
         return False
