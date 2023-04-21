4258c5c32038b4ba837c2e986ce608d41f4e2b60
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-04-21 18:49:11 +1200

    Adding binary search to problem 704

--- 704.Binary-Search/binary-search.js
+++ 704.Binary-Search/binary-search.js
@@ -2,9 +2,30 @@
 // If target exists, then return its index. Otherwise, return -1.
 // You must write an algorithm with O(log n) runtime complexity.
 
+// SOLUTION 1:
+// Although this solution does the job, it runs in O(n) and not in O(log n)
 const search = function(nums, target) {
     const targetIndex = nums.indexOf(target)
     return (targetIndex)
+}
+
+// SOLUTION 2:
+// Using Binary Search
+const search2 = function(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
 }
 
 const nums = [-1,0,3,5,9,12];
