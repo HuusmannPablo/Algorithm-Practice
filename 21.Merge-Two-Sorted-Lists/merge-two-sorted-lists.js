239b9f2440fe794fb94380c8a8e348a05824f748
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-05-09 16:30:07 +1200

    First part of the 21 problem solved

--- 21.Merge-Two-Sorted-Lists/merge-two-sorted-lists.js
+++ 21.Merge-Two-Sorted-Lists/merge-two-sorted-lists.js
@@ -4,8 +4,6 @@
 
 // Return the head of the merged linked list.
 
- 
-
 // Example 1:
 // Input: list1 = [1,2,4], list2 = [1,3,4]
 // Output: [1,1,2,3,4,4]
@@ -19,5 +17,24 @@
 // Output: [0]
 
 const mergeTwoLists = function(list1, list2) {
+    
+    // I initialize a new list, and the head of that new list.
+    // I will put all the values in this new list, in order
+    let newList = new ListNode();
+    let head = newList;
+
+    // If I'm given two lists, then I'll check which value is lower (list 1 or 2) and insert that into the new list
+    // then move to the next node on that list, and compare again. 
+    while(list1 && list2) {
+        if(list1.val <= list2.val) {
+            newList.next = new ListNode(list1.val);
+            list1 = list1.next;
+        } else {
+            newList.next = new ListNode(list2.val);
+            list2 = list2.next;
+        }
+        newList = newList.next;
+    }
+
     
 };
