e73a7cc61c40b90963018c270140af1ce4536075
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-05-11 18:40:24 +1200

    Problem 206: solution in JS added

--- 206.Reverse-Linked-List/reverse-linked-list.js
+++ 206.Reverse-Linked-List/reverse-linked-list.js
@@ -14,4 +14,24 @@
 
 const reverseList = function(head) {
     
+    // I initialize the new linked list that will have the reversed items
+    // and set the value of current to head
+    let newList = null;
+    let current = head;
+
+    // While there is a node in the head
+    while(current !== null){
+
+        // create a new node and set its value to the current node value of the head linked list
+        let newNode = new ListNode(current.val);
+
+        // add the value of this new node to the beginning of the new list
+        newNode.next = newList;
+        // update the newList to be the newNode
+        newList = newNode;
+        // Move to the next node on the original list
+        current = current.next
+    }
+    // return the new list that contains the reversed elements
+    return newList
 };
