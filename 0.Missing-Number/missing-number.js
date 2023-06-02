bd1ea1108cae3eb0227f3560723fc6f3639a6593
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-06-02 16:02:36 +1200

    Problem 0, suggested by Adan presented

---
+++ 0.Missing-Number/missing-number.js
@@ -0,0 +1,7 @@
+// Find the Missing Number:
+// Write a function that takes an array of integers from 1 to n (with one number missing) and returns the missing number.
+// For example, if the input is [1, 2, 4, 5, 6], the function should return 3.
+
+const findingMissingNumber = function (inputArray) {
+
+}
