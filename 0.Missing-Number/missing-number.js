7294d9710d86e11efad3bd5d1115ee6ece172ea0
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-06-02 16:22:14 +1200

    Problem 0: solved, working on edge cases

--- 0.Missing-Number/missing-number.js
+++ 0.Missing-Number/missing-number.js
@@ -2,6 +2,16 @@
 // Write a function that takes an array of integers from 1 to n (with one number missing) and returns the missing number.
 // For example, if the input is [1, 2, 4, 5, 6], the function should return 3.
 
-const findingMissingNumber = function (inputArray) {
-
+function findingMissingNumber(inputArray) {
+    if(inputArray == []) {
+        return `The input array is empty`
+    }
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i + 1] - inputArray[i] === 2) return inputArray[i] + 1;
+    }
+    return `The array has no missing numbers`;
 }
+
+console.log(findingMissingNumber([1,2,3,4,5,7,8,9]))        // Should retrun 6
+console.log(findingMissingNumber([1,2,3,4,5,6,7,8,9]))      // Should retrun The array has no missing numbers
+console.log(findingMissingNumber([]))                       // Should retrun The input array is empty
