f376164aef2938b754f75f7985300dae3d87dda9
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-06-20 20:13:33 +1200

    Problem 347: pseudocoded solution in progress

--- 347.Top-K-Frequent-Elements/top-k-frequent-elements.js
+++ 347.Top-K-Frequent-Elements/top-k-frequent-elements.js
@@ -10,7 +10,32 @@
 // Output: [1]
 
 const topKFrequent = function(nums, k) {
+    // Creat an object to store numbers and frequencies
+    const numbersFrequencies = {};
+
+    // Loop through the nums array 
+
+    // if the number exists in the object, add 1 to the count
+
+    // else, create a new key with the value and put freq = 1
+
+    // after that, sort the key:values from largest to smallest
+    // Object.entries(obj).sort(function large to small);
+    // I get an array of arrays with [[number, freq], [number2, freq2]...]
     
+    // Option 1: Return the k most frequent numbers:
+    
+
+
+
+    // Option 2: re-convert to object, but sorted
+    // then Object.fromEntries(sortedObject);
+    // this returns an object of key:values with everything sorted. 
+    // The numbers with higher freq will be the firstones
+
+
+
+
 };
 
 
