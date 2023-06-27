c595d705b69e8f359c0221fd64bf1ccbf0230363
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-06-28 10:51:37 +1200

    Problem 36: first lines of code for row checking

--- 36.Valid-Sudoku/valid-sudoku.js
+++ 36.Valid-Sudoku/valid-sudoku.js
@@ -22,7 +22,8 @@
 // Output: true
 
 const isValidSudoku = function(board) {
-    
+   
+// SOLUTION 1: 
 // I could loop through the rows and columns checking every value
 // for the first value:
     // check its not present in the rest of the row
@@ -31,6 +32,29 @@ const isValidSudoku = function(board) {
     // box 1: 0-2
     // box 2: 3-5
     // box 3: 6-8
+
+// SOLUTION 2:
+// I should check the rows
+    // I can create a Set and put the numbers there, and check that they are not there already
+    // keep going if they are not there, return false if thay are
+
+// I should check the columns then
+    // I can create a Set and put the numbers there, and check that they are not there already
+    // keep going if they are not there, return false if thay are
+
+// I should check the 3x3 boxes
+// Need to figure out how to identify each box
+// and use the same Set procedure as in the ohter two checks
+
+    for(let i = 0; i < 9; i++) {
+
+        // create a Set
+        for(let j = 0; j < 9; j++) {
+            // if(there is a number && that number is in the set)
+                return false
+            // else, add the number to the set
+        }
+    }
 
 
 };
