72870eb357a103289ae9292939cf58f2cced7716
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-06-28 10:34:15 +1200

    Problem 36: Pseudocoding solutions

--- 36.Valid-Sudoku/valid-sudoku.js
+++ 36.Valid-Sudoku/valid-sudoku.js
@@ -23,6 +23,16 @@
 
 const isValidSudoku = function(board) {
     
+// I could loop through the rows and columns checking every value
+// for the first value:
+    // check its not present in the rest of the row
+    // check its not present in the rest of the column
+    // check its not present in the box (depending of its index to identify which box I shoild check)
+    // box 1: 0-2
+    // box 2: 3-5
+    // box 3: 6-8
+
+
 };
 
 const board = [
