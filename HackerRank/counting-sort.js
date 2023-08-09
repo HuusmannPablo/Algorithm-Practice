31caafc309ae109052fc958624a9cc12470a2f06
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-08-09 19:51:05 -0300

    HackerRank excercise 6: Counting Sort solved, output needs to be a string, not an array

--- HackerRank/counting-sort.js
+++ HackerRank/counting-sort.js
@@ -1,12 +1,17 @@
 // Comparison Sorting
 // Quicksort usually has a running time of n x log(n), but is there an algorithm that can sort even faster? 
-// In general, this is not possible. Most sorting algorithms are comparison sorts, i.e. they sort a list just by comparing the elements to one another. 
-// A comparison sort algorithm cannot beat n x log(n) (worst-case) running time, since n x log(n) represents the minimum number of comparisons needed to know where to place each element. 
+// In general, this is not possible. Most sorting algorithms are comparison sorts, 
+// i.e. they sort a list just by comparing the elements to one another. 
+// A comparison sort algorithm cannot beat n x log(n) (worst-case) running time, since n x log(n) represents 
+// the minimum number of comparisons needed to know where to place each element. 
 // For more details, you can see these notes (PDF).
+// http://www.cs.cmu.edu/~avrim/451f11/lectures/lect0913.pdf
 
 // Alternative Sorting
 // Another sorting method, the counting sort, does not require comparison. 
-// Instead, you create an integer array whose index range covers the entire range of values in your array to sort. Each time a value occurs in the original array, you increment the counter at that index. At the end, run through your counting array, printing the value of each non-zero valued index that number of times.
+// Instead, you create an integer array whose index range covers the entire range of values in your array to sort. 
+// Each time a value occurs in the original array, you increment the counter at that index. 
+// At the end, run through your counting array, printing the value of each non-zero valued index that number of times.
 
 // Example
 // arr = [1, 1, 3, 2, 1]
@@ -48,5 +53,13 @@
 
 function countingSort(arr) {
     // Write your code here
-
+    let frequencyArray = [];
+    for(let j = 0; j < 100; j++) {
+        frequencyArray.push(0);
+    };
+    for (let i = 0; i < arr.length; i++) {
+        let valueInArr = arr[i];
+        frequencyArray[valueInArr] += 1;
+    };
+    console.log(frequencyArray);
 };
