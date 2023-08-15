3b5fe13006d574aca42f9084fa3abfaf08c74638
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-08-15 13:37:41 -0300

    HackerRank excercise 10: recursive digit solved in two ways

--- HackerRank/recursive-digit.ts
+++ HackerRank/recursive-digit.ts
@@ -25,8 +25,39 @@
 // All of the digits of p sum to 116. The digits of 116 sum to 8. 8 is only one digit, so it is the super digit.
 
 function superDigit(n: string, k: number): number {
-    // Write your code here
-    let fullString: string = n.repeat(k);
     
-
+    // SOLUTION 2: This one works and doesn't have the parseInt limitation
+    if (n.length === 1) {
+        return parseInt(n);
+    } else {
+        let sum = 0;
+        for (const digit of n) {
+            sum += parseInt(digit);
+        }
+        const newNumber = (sum * k).toString();
+        return superDigit(newNumber, 1);
+    };
 };
+
+//     SOLUTION 1: this one has the issue of parseInt having a limit of 18 characters of presicion.
+//     console.log(n, k);
+//     let fullString: string = n.repeat(k);
+//     console.log(fullString);
+//     let fullNumber: number = parseInt(fullString, 10);
+//     console.log(fullNumber);
+    
+//     while(fullNumber > 9) {
+//         fullNumber = sumDigits(fullNumber);
+//         console.log(fullNumber);
+//     };
+//     return fullNumber;
+// };
+
+// function sumDigits(n: number): number {
+//     let sum: number = 0;
+//     while (n > 0) {
+//         sum += n % 10;
+//         n = Math.floor(n / 10);
+//     };
+//     return sum;
+// };
