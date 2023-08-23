47a321786a8f4937351629993f173a00bf915607
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-08-23 20:01:42 -0300

    HackerRank excercise 16: Simple Text Editor presented

---
+++ HackerRank/simple-text-editor.ts
@@ -0,0 +1,21 @@
+// Implement a simple text editor. 
+// The editor initially contains an empty string, S. 
+// Perform Q operations of the following 4 types:
+
+// 1. append(W) - Append string W to the end of S.
+// 2. delete(k) - Delete the last k characters of S.
+// 3. print(k) - Print the kth character of S.
+// 4. undo() - Undo the last (not previously undone) operation of type 1 or 2, reverting S to the state it was in prior to that operation.
+
+// Input Format
+// The first line contains an integer, Q, denoting the number of operations.
+// Each line i of the Q subsequent lines (where 0 <= i < Q) defines an operation to be performed.
+// Each operation starts with a single integer, t (where t ∈ {1, 2, 3, 4}), 
+// denoting a type of operation as defined in the Problem Statement above.
+// If the operation requires an argument, t is followed by its space-separated argument.
+// For example, if t = 1 and W = 'abcd', line i will be 1 abcd.
+
+function processData(input: string[]) {
+    //Enter your code here
+    
+} 
