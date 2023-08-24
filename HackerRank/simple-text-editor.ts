b5edc2f5bda98f8ac7f03c1ee703d23368a37f17
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-08-23 22:04:07 -0300

    HackerRank excercise 16: solution working and commented

--- HackerRank/simple-text-editor.ts
+++ HackerRank/simple-text-editor.ts
@@ -15,73 +15,53 @@
 // If the operation requires an argument, t is followed by its space-separated argument.
 // For example, if t = 1 and W = 'abcd', line i will be 1 abcd.
 
-function processData(input: string) {
-    //Enter your code here
-    // make a copy of S in case I need to revert to last S
-    // 1 = append or concat string
-    // 2 = delete, split string at k character and get rid of second part
-    // 3 = print, console.log(S[k-1])
-    // 4 = undo, S = Sprior
-
-    // create a stack of S strings. I can add to the stack when I append or delete
-    // and add 1 to a counter. When I undo, I can pop the stack and subtract 1 from the counter
-    // this needs to solve the issue of getting consecutive undo commands
+function textEditor(input: string) {
     
-
-    let S = '';
-    input = input.split('\n');
-    let Q = parseInt(input[0]);
+    // Initiallize the string S
+    let S: string = '';
+    // Transform the input into an array
+    let inputArray: string[] = input.split('\n');
+    // Get the number of operations
+    let Q = parseInt(inputArray[0]);
+    // Create a stack to store the string S, and an index to keep track of the stack
     let stack = [S];
     let index = 0;
 
+    // loop through the operations
     for (let i = 1; i <= Q; i++) {
-        if (input[i][0] === '1') {
-          S = S.concat(input[i].slice(2));
-          stack.push(S);
-          index++;
-        } else if (input[i][0] === '2') {
-          S = S.slice(0, S.length - parseInt(input[i].slice(2)));
-          stack.push(S);
-          index++;
-        } else if (input[i][0] === '3') {
-          console.log(S[parseInt(input[i].slice(2)) - 1]);
-        } else if (input[i][0] === '4') {
-          if (index > 0) {
-            stack.pop();
-            index--;
-            S = stack[index];
+
+        // If the operation is 1
+        if (inputArray[i][0] === '1') {
+            // Append the string to S
+            S = S.concat(inputArray[i].slice(2));
+            // Push the new S to the stack
+            stack.push(S);
+            // Increment the index
+            index++;
+        
+        // If the operation is 2
+        } else if (inputArray[i][0] === '2') {
+            // Delete the last k characters of S
+            S = S.slice(0, S.length - parseInt(inputArray[i].slice(2)));
+            stack.push(S);
+            index++;
+        
+        // If the operation is 3
+        } else if (inputArray[i][0] === '3') {
+            // Print the kth character of S
+            console.log(S[parseInt(inputArray[i].slice(2)) - 1]);
+        
+        // If the operation is 4
+        } else if (inputArray[i][0] === '4') {
+            // Undo the last operation
+            if (index > 0) {
+                // remove the last element of the stack
+                stack.pop();
+                // decrement the index
+                index--;
+                // set S to the last element of the stack
+                S = stack[index];
           };
         };
     };
-
-
-        // let [operationType, k] = input[i].split(' ');
-        // if (operationType === '1') {
-        //     Sprior = S;
-        //     S = S.concat(k);
-        // } else if (operationType === '2') {
-        //     Sprior = S;
-        //     S = S.slice(0, S.length - parseInt(k));
-        // } else if (operationType === '3') {
-        //     console.log(S[parseInt(k) - 1]);
-        // } else if (operationType === '4') {
-        //     S = Sprior;
-        // };
-    };
-    // let i = 1;
-    // while (i <= Q) {
-    //     let [t, k] = input[i].split(' ');
-    //     if (t === '1') {
-    //         Sprior = S;
-    //         S = S.concat(k);
-    //     } else if (t === '2') {
-    //         Sprior = S;
-    //         S = S.slice(0, S.length - parseInt(k));
-    //     } else if (t === '3') {
-    //         console.log(S[parseInt(k) - 1]);
-    //     } else if (t === '4') {
-    //         S = Sprior;
-    //     }
-    //     i++;
-    // };
-}; 
+};
